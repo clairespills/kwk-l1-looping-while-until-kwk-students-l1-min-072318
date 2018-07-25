11d8a0a209633ee4b42e59b74faf27a47b1815df
clairespills <purpleprincess372@gmail.com>
2018-07-25 17:58:07 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
-        levitation_force = 6
-        #your code here
+   levitation_force = 6
+  until time == 24
+  puts "Wake up idiot!"
+  time += 4
+end
+end
+
+alarm_clock_claire
     
 end
 
